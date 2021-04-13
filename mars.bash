14b6f1d49bfbd999e65e6912e22adf4bae761c25
Serhii Hiba <enotikalt@gmail.com>
2021-04-13 17:58:26 +0000

    reload terminal deleted

--- mars.bash
+++ mars.bash
@@ -1,14 +1,9 @@
 #! /bin/bash
 
-function reload_terminal(){
-	source $HOME/.bashrc
-}
-
   echo "Mars initializing..."
   
   if ! grep -q "source ~/.mars/lib/aliases.bash" "$HOME/.bashrc" ; then
     if echo "source ~/.mars/lib/aliases.bash" >> $HOME/.bashrc ; then 
-      reload_terminal
 	    echo "[OK] Mars connected successfully!"
     else
       echo "[ERROR] Couldn't connect Mars to your .bashrc"
