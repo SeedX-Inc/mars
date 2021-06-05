3967c1d7efef196948ff9686a99f010c84ef2466
Serhii Hiba <enotikalt@gmail.com>
2021-06-05 16:10:06 +0300

    PS1 updates

--- lib/ps1.bash
+++ lib/ps1.bash
@@ -1,5 +1,11 @@
 function gemset_name(){
-  rvm gemset name
+  string=$(rvm gemset name)
+  # the same to string=`rvm gemset name`
+  if [[ $string == *"/"* ]]; then
+    return "default (maybe)"
+  else
+    return $string
+  fi
 }
 
 export PS1='\[\e[30;102m\] \u \[\e[m\] \[\e[37;101m\] $(gemset_name)💎 \[\e[m\] \[\e[1;96m\]\w\[\e[m\] ⚡ '
