46ba62f39911969c6a7fc1957a0ea1285ea99006
Serhii Hiba <enotikalt@gmail.com>
2021-06-05 19:57:29 +0300

    quicktest

--- lib/mod_conf.bash
+++ lib/mod_conf.bash
@@ -10,10 +10,10 @@ function configure_sidekiq(){
     vim /lib/systemd/system/sidekiq.service #edit sidekiq file
   else
 
-    sudo bash << EOF
-    echo 'Hello World'
-    sudo cp $HOME/.mars/perks/sidekiq.service /lib/systemd/system/sidekiq.service
-    EOF
+sudo bash << EOF
+  echo 'Hello World'
+  sudo cp $HOME/.mars/perks/sidekiq.service /lib/systemd/system/sidekiq.service
+EOF
 
     if [[ -f /lib/systemd/system/sidekiq.service ]]; then
       echo "[OK] sidekiq.service file copied successfully."
